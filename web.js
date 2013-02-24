f62b29cde84d0105714172ba1bfff7d21d6e93c7
Wesley Leung <Wes.k.Leung@gmail.com>
2013-02-23 21:34:14 -0800

    app port update

--- web.js
+++ web.js
@@ -34,10 +34,6 @@ app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
 
-
-var port = process.env.PORT || 3000;
-
-
-app.listen(port, function(){
+app.listen(app.get('port'), function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
