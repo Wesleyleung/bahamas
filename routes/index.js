4a56a8f908bdec433db572e716c2511d4c5dc8d1
Wesley Leung <Wes.k.Leung@gmail.com>
2013-02-23 21:02:42 -0800

    mvc setup and ability to service rest apis

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+
+/*
+ * GET about page 
+ */
+ exports.about = function(req,res) {
+ 	res.render('about', {title: 'About'})
+ };
+
+ /*
+ * GET contact page
+ */
+ exports.contact = function(req,res) {
+ 	res.render('contact', {title: 'Contact'})
+ };
