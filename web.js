c3710bffe4e727e08746ab98a2e761ac29b63fda
John Yang-Sammataro <johnys@stanford.edu>
2013-03-02 23:21:09 -0800

    updated modules and fixed address issue for latest express version

--- web.js
+++ web.js
@@ -6,7 +6,7 @@
 var express = require('express')
   , routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var app = express();
 
 // Configuration
 
@@ -36,5 +36,5 @@ app.get('/about', routes.about);
 app.get('/contact', routes.contact);
 
 app.listen(app.get('port'), function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
