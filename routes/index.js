7316ed79bb1875e3a7f522847c2d298f8ba52cb2
John Yang-Sammataro <johnys@stanford.edu>
2013-03-08 18:43:57 -0800

    fiddling with buttons

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,26 @@
  * GET home page.
  */
 exports.index = function(req, res) {
-  res.render('index', { title: 'Stanford Fixit' })
+  res.render('index', { title: 'Stanford Fixit',	title_link: '/',
+												left_title: 'About',			left_link: '/about',
+												right_title: 'Contact',		right_link: '/contact'})
 };
 
 /*
  * GET about page 
  */
 exports.about = function(req,res) {
-	res.render('about', {title: 'About'})
+	res.render('about', { title: 'About',	title_link: '/about',
+												left_title: 'Home',			left_link: '/',
+												right_title: 'Contact',		right_link: '/contact'})
+
 };
 
  /*
  * GET contact page
  */
 exports.contact = function(req,res) {
-	res.render('contact', {title: 'Contact'})
+	res.render('contact', { title: 'Contact',	title_link: '/contact',
+												left_title: 'Home',			left_link: '/',
+												right_title: 'About',		right_link: '/about'})
 };
