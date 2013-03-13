7724882fe3345bc290d0c2d924541f8d90792653
Diana Lee <diana.yiyng@gmail.com>
2013-03-13 12:30:34 -0700

    access to location

--- public/js/index.js
+++ public/js/index.js
@@ -11,5 +11,24 @@ function showPreview(input) {
 			//document.getElementById('hidden').style.display="none";
 		}
 		reader.readAsDataURL(input.files[0]);
+
+		function success(position) {
+			var lat = position.coords.latitude;
+			var lng = position.coords.longitude;
+			$('#location')[0].value = lat;
+			console.log(lat);
+			//document.getElementById('photo_lng').value = lng;
+		}
+
+		function error(msg) {
+			var errMsg = typeof msg == 'string' ? msg : "Geolocation failed";
+			$('#msg').html(errMsg);
+		}
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, error);
+		} else {
+			error('Geolocation not supported');
+		}
 	}
 }
