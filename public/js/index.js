eedbe236f4d86f13b36c8370dfc40fd878b7b731
Diana Lee <diana.yiyng@gmail.com>
2013-03-13 12:19:38 -0700

    js for photo preview

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,15 @@
 function divClickFunction() {
 	$('#upload_input').click();
 }
+
+function showPreview(input) {
+	if (input.files && input.files[0]) {
+	    var reader = new FileReader();
+		reader.onload = function(e) {
+			$('#pic').attr('src', e.target.result);
+			//document.getElementById('hiddenPreview').style.display="block";
+			//document.getElementById('hidden').style.display="none";
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
