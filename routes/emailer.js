2c96168bfe1deef73f42290167ddeedcc824d39f
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-13 20:32:07 -0700

    heroku test

--- routes/emailer.js
+++ routes/emailer.js
@@ -1,26 +1,38 @@
+// var SendGrid = require('sendgrid').SendGrid;
+// var bahamasfixit_email_address = "bahamasfixit@gmail.com";
 
 
+// //Test function. Fields should be extracted from form.
+// exports.email = function(req, res) {
+// 	var user;
+// 	var key;
+// 	console.log(process.env.SENDGRID_USERNAME);
+// 	console.log(process.env.SENDGRID_PASSWORD);
+// 	if(!process.env.SENDGRID_USERNAME) {
+// 		user = "";
+// 	} else {
+// 		user = process.env.SENDGRID_USERNAME;
+// 	}
+// 	if(!process.env.SENDGRID_PASSWORD) {
+// 		key = "";
+// 	} else {
+// 		key = process.env.SENDGRID_PASSWORD;
+// 	}
 
-var SendGrid = require('sendgrid').SendGrid;
-var sendgrid = new SendGrid(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
-var bahamasfixit_email_address = "bahamasfixit@gmail.com";
+// 	var sendgrid = new SendGrid(user, key);
+// 	sendgrid.send({
+//   		to: 'wes.k.leung@gmail.com',
+//   		from: bahamasfixit_email_address,
+//   		subject: 'Hello World',
+//   		text: 'Sending email with NodeJS through SendGrid!'
+// 	}, function(success, message) {
+// 		if(!success) {
+// 			console.log(message);
+// 		} 
+// 		console.log(success);
+// 	});
 
-
-//Test function. Fields should be extracted from form.
-exports.email = function(req, res) {
-	console.log(process.env.SENDGRID_USERNAME);
-	sendgrid.send({
-  		to: 'wes.k.leung@gmail.com',
-  		from: bahamasfixit_email_address,
-  		subject: 'Hello World',
-  		text: 'Sending email with NodeJS through SendGrid!'
-	}, function(success, message) {
-		if(!success) {
-			console.log(message);
-		}
-	});
-
-	res.render('success', { title: 'Success', title_link: '/success',
-                          left_title: 'Home', left_link: "/"})
-}
+// 	res.render('success', { title: 'Success', title_link: '/success',
+//                           left_title: 'Home', left_link: "/"})
+// }
 
