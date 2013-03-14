65e55ae1308af5e4660b7ceabd19e7fec0393ca3
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-13 19:50:35 -0700

    sendgrid email route test to web.js

--- routes/emailer.js
+++ routes/emailer.js
@@ -8,12 +8,16 @@ var bahamasfixit_email_address = "bahamasfixit@gmail.com";
 
 //Test function. Fields should be extracted from form.
 exports.email = function(req, res) {
-	
+	console.log(process.env.SENDGRID_USERNAME);
 	sendgrid.send({
   		to: 'wes.k.leung@gmail.com',
   		from: bahamasfixit_email_address,
   		subject: 'Hello World',
   		text: 'Sending email with NodeJS through SendGrid!'
+	}, function(success, message) {
+		if(!success) {
+			console.log(message);
+		}
 	});
 
 	res.render('success', { title: 'Success', title_link: '/success',
