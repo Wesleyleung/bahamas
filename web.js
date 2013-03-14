4650ed68c6ff1f01349fa841768772e1a10f3ad6
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-13 23:50:05 -0700

    hotfix of routes to heroku

--- web.js
+++ web.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes  = require('./routes')
-  // , emailer = require('./routes/emailer')
+  , emailer = require('./routes/emailer')
   , path    = require('path');
 
 var app = express();
@@ -44,7 +44,7 @@ app.get('/about', routes.about);
 app.get('/contact', routes.contact);
 app.get('/information', routes.information);
 app.get('/success', routes.success);
-// app.get('/email', emailer.email);
+app.get('/email', emailer.email);
 
 app.listen(app.get('port'), function() {
   console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
