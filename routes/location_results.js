64665fb86b3672200de919f0ae79c293a80af5ef
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-20 22:37:42 -0700

    added heroku toolbelt and uri

--- routes/location_results.js
+++ routes/location_results.js
@@ -1,9 +1,20 @@
-exports.fetchLocations = function(req, res) {
-	var db = require('mongoskin').db('localhost/dormLocationDB');
+var mongo = require('mongodb');
 
-	db.collection('locations').find().sort({name:1}).toArray(function(err, result) {
-		if (err) throw err;
-		console.log(result);
-		res.send(result);
+
+
+exports.fetchLocations = function(req, res) {
+
+
+    var mongoUri = process.env.MONGOLAB_URI || 
+  		process.env.MONGOHQ_URL || 
+  		'localhost/dormLocationDB'; 
+
+
+  	mongo.Db.connect(mongoUri, function (err, db) {
+		db.collection('locations').find().sort({name:1}).toArray(function(err, result) {
+			if (err) throw err;
+			console.log(result);
+			res.send(result);
+		});
 	});
 };
